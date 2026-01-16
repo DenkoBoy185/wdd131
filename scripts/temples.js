af366be6428fef318f58e2c80f622858260413fd
DenkoBoy185 <redhood185.jasontod@gmail.com>
2026-01-15 23:20:35 -0700

    Add files via upload

---
+++ scripts/temples.js
@@ -0,0 +1,15 @@
+// Footer Data
+const currentYear = new Date().getFullYear();
+const lastModified = document.lastModified;
+
+document.getElementById('currentyear').textContent = currentYear;
+document.getElementById('lastModified').textContent = `Last Modification: ${lastModified}`;
+
+// Hamburger Menu
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('nav');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('show');
+    hamButton.classList.toggle('open');
+});
